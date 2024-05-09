527807c2d39b469390ad0b951a3801025f33c235
Hrebynakha Anatolii <hrebynakha@gmail.com>
2024-05-09 21:57:08 +0300

    upd hw2 and text

--- hw_5_2.py
+++ hw_5_2.py
@@ -10,17 +10,21 @@
 
 def binary_search(arr, x, depth=1):
     """Binary recursive search"""
-    array_length = len(arr)
-    mid = array_length // 2
+    mid = len(arr) // 2
+    #print(arr, mid, arr[mid])
     if arr[mid] == x:
-        return x, depth
-    if array_length == 1:
-        return None, depth
-    new_array = arr[:mid] if arr[mid] > x else arr[mid:]
+        return depth, arr[mid]
+    if len(arr) == 2:
+        # return right or left element for x
+        if x > arr[mid-1]:
+            return depth, arr[mid]
+        return depth, arr[mid-1]
+    new_array = arr[:mid+1] if arr[mid] > x else arr[mid:]
     return binary_search(new_array, x, depth=depth+1)
 
-example_array = [1, 2, 2.6, 2.8, 3, 9, 10.5, 10.7, 19.887, 19.888, 20, 21.6]
+arr  = [1.1, 1.3, 2.5, 3.8, 4.6, 5.9]
 
 if __name__ == '__main__':
-    element = float(input("Input element to search:"))
-    print(binary_search(example_array, element))
+    for test_element in [3.8, 4.6, 6.0, 1.2, 3, 0, 10 ]:
+        print("Searching element...", test_element)
+        print(binary_search(arr, test_element))
